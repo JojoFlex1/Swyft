75917f3cb4017fa82da90c25119accafe8e22247
dreamgenies <dreamgenies@github.com>
2026-04-28 08:47:06 +0100

    feat(sdk): add local swap quote calculation

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,10 @@
-export { calculateSwapQuote } from "./quote";
-export type { SwapQuoteParams, SwapQuote } from "./quote";
+export { calculateSwapQuote, getSwapQuote } from "./quote";
+export type {
+  LocalSwapQuote,
+  LocalSwapQuoteParams,
+  SwapQuote,
+  SwapQuoteParams,
+} from "./quote";
 
 export { buildBurnTx, buildCollectTx, estimateRemoveAmounts } from "./liquidity";
 export type { BurnTxParams, CollectTxParams, UnsignedTx } from "./liquidity";
